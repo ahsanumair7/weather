b064f3889a57aa835353e076a69db37076ae3387
Ahsan Umair <167495417+ahsanumair7@users.noreply.github.com>
2024-08-28 15:12:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from src.agent.io_interface import (
     SynchronousTTT,
     SharedValue,
 )
-from src.system_conf import (
-    REPEAT_PROMPT,
-    DYNAMIC_INTERRUPTION,
-)
 import os
 import json
 from src.main import AgentWorker
@@ -22,6 +18,8 @@ import requests
 
 STEP_ONE = "Which specific location are you interested in knowing the weather for?"
 STEP_TWO = "Are you sure"
+REPEAT_PROMPT = "I'm sorry, I didn't get that. Please repeat that."
+DYNAMIC_INTERRUPTION = "dynamic_interruption"
 
 
 class CheckWeatherCapability(MatchingCapability):
@@ -121,15 +119,15 @@ class CheckWeatherCapability(MatchingCapability):
 
     def call(
         self,
-        msg: str,
-        agent: BotAgent,
-        text_respond: SynchronousTTT,
-        speak_respond: None,
-        audio: str,
         worker: AgentWorker,
-        meta: dict[str, Any],
         interrupt_str: SharedValue,
     ):
+        msg = worker.final_user_input
+        agent = worker.agent
+        text_respond = worker.ttt_sync
+        speak_respond = worker.tts_ios
+        audio = "/tmp/the_file.wav"
+        meta = {}
         os.environ[DYNAMIC_INTERRUPTION] = "False"
 
         self.worker = worker
