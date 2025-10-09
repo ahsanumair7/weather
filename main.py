57e15448f7e04cc2967b8fcab50ef425cae52186
Ahsan Umair <167495417+ahsanumair7@users.noreply.github.com>
2025-10-09 16:31:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import os
 from src.agent.capability import MatchingCapability
 from src.main import AgentWorker
 from src.agent.capability_worker import CapabilityWorker
@@ -15,16 +14,7 @@ class WeatherCapability(MatchingCapability):
     capability_worker: CapabilityWorker = None
     weather_report: str = ""
 
-    @classmethod
-    def register_capability(cls) -> "MatchingCapability":
-        with open(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
-        ) as file:
-            data = json.load(file)
-        return cls(
-            unique_name=data["unique_name"],
-            matching_hotwords=data["matching_hotwords"],
-        )
+    #{{register capability}}
 
     def get_location(self, answer: str):
         geolocator = Nominatim(user_agent="my_user_agent")
